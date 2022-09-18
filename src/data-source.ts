bc15e7b25a7551f6eff3650eeb660bc3d1cb7fe1
revoyrm <makr28@gmail.com>
2022-09-18 13:08:42 -0400

    book entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,15 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
-
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "sa",
+  database: "library",
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: [],
+});

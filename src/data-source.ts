983a38214eae0b54413a2b6fd5f7f317991359e0
revoyrm <makr28@gmail.com>
2023-02-04 14:55:52 -0500

    start

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,7 +5,7 @@ export const AppDataSource = new DataSource({
   host: "localhost",
   port: 5432,
   username: "postgres",
-  password: "sa",
+  password: "password",
   database: "library",
   synchronize: true,
   logging: false,
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import { AppDataSource } from "./data-source";
-import { User } from "./entity/Book";
+import { Book } from "./entity/Book";
 
 AppDataSource.initialize()
   .then(async () => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await AppDataSource.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    console.log("Inserting a new Book into the database...");
+    const book = new Book();
+    book.Title = "Timber";
+    book.author = "Avery Fay";
+    await AppDataSource.manager.save(book);
+    console.log("Saved a new user with id: " + book.id);
 
     console.log("Loading users from the database...");
-    const users = await AppDataSource.manager.find(User);
-    console.log("Loaded users: ", users);
+    const books = await AppDataSource.manager.find(Book);
+    console.log("Loaded users: ", books);
 
     console.log(
       "Here you can setup and run express / fastify / any other framework."

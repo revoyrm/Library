51dde78308a59e38cccb5c5ed534b5f867006e5d
revoyrm <makr28@gmail.com>
2023-03-05 20:27:18 -0500

    initial database

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers } from "./resolvers";
 import { typeDefs } from "./schema";
+import { AppDataSource } from "./data-source";
 
 const startServer = async () => {
   const server = new ApolloServer({
@@ -10,20 +11,13 @@ const startServer = async () => {
     resolvers,
   });
 
+  await AppDataSource.initialize();
+
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
   });
 
   console.log(`🚀  Server ready at: ${url}`);
-  // await createConnection();
-
-  // const app = express();
-
-  // server.applyMiddleware({ app });
-
-  // app.listen({ port: 4000 }, () =>
-  //   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-  // );
 };
 
 startServer();
---
+++ src/migrations/1678065417183-initial.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class initial1678065417183 implements MigrationInterface {
+    name = 'initial1678065417183'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "book" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "author" character varying NOT NULL, "summary" character varying NOT NULL, CONSTRAINT "PK_a3afef72ec8f80e6e5c310b28a4" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "book"`);
+    }
+
+}
